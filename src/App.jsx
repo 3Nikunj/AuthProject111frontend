676021084976f7178652b4e443aefcf1e44fc9ef
3nikunj <3nikunjsondawale@gmail.com>
2025-12-25 21:30:26 +0530

    frotend

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Link, Route, Router, Routes } from "react-router-dom";
 
-import Register from "./components/register";
-import Login from "./components/login";
-import Dashboard from "./components/dashboard";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
